027e3a704098f5d189791823bf83b27902be93e4
nathanwilk7 <nathanwilk7@gmail.com>
2017-04-19 13:58:43 -0600

    cleaned up workhorse func

--- gsv.py
+++ gsv.py
@@ -391,6 +391,9 @@ def split_by_breakpoint_old (read_list, left_pos, right_pos, split_slop):
                     if read2.reference_end>= left_pos - split_slop and read2.reference_start<= left_pos + split_slop:
                         return True
     return False
+
+# Save this because i may need to go back to SA tags in the end...
+
 """
         if read.has_tag('SA'):
             split_left = read.reference_end >= left_pos - split_slop and read.reference_end <= left_pos + split_slop
@@ -1123,28 +1126,12 @@ def genotype_variants (args):
         sv_info.chrom_length = input_bam.lengths[input_bam.gettid(variant.CHROM)]
 
         variant_features = VariantFeatures()
-        # Get variant info into local vars
-        #chrom = variant.CHROM
-        #svtype = variant.INFO.get('SVTYPE')
-        #left_pos = variant.start
-        #right_pos = variant.end # variant.INFO.get('END')
-        #left_conf_int = variant.INFO.get('CIPOS')
-        #right_conf_int = variant.INFO.get('CIEND')
-        #chrom_length = input_bam.lengths[input_bam.gettid(chrom)]
 
         # Get the reads around the variant on both sides
         reads = fetch_reads(input_bam, sv_info, options)
 
         # Get the reads in the middle of the variant
-        #inner_reads = fetch_inner_reads(input_bam, svtype, chrom, left_pos, left_conf_int, 
-        #                                right_pos, right_conf_int, INNER_READ_FETCH_FLANK)
         inner_reads = fetch_inner_reads(input_bam, sv_info, options)
-
-        # Uses coverage and aligned bases to find coverage depth
-        #sv_info.left_lower_bound, sv_info.left_upper_bound = get_query_bounds(svtype, left_pos, left_conf_int, FETCH_FLANK, chrom_length)
-        #sv_info.right_lower_bound, sv_info.right_upper_bound = get_query_bounds(svtype, right_pos, right_conf_int, FETCH_FLANK, chrom_length)
-        #ref_bases_covered_left_actual, ref_bases_covered_right_actual = 0, 0
-        #ref_left_read_count, ref_right_read_count = 0, 0
 
         # Save reads that span breakpoint
         reads_span_breakpoint = set()
@@ -1169,7 +1156,6 @@ def genotype_variants (args):
         # functions that need the fasta file to work
         fasta_funcs = []
 
-        #pdb.set_trace()
         # Extract support from individual reads
         for read_list in reads.values():
             for read in read_list:
@@ -1215,68 +1201,6 @@ def genotype_variants (args):
                 alt_read_count += 1
                 for inner_read_func in inner_read_funcs:
                     inner_read_func(variant_features, inner_read, sv_info, options)
-
-                #if spans_breakpoint(read, left_pos, left_conf_int, MIN_ALIGNED, MIN_PCT_ALIGNED):
-                #    ref_support += 0.5
-                #    reads_span_breakpoint.add(read)
-
-                #if spans_breakpoint(read, right_pos, right_conf_int, MIN_ALIGNED, MIN_PCT_ALIGNED):
-                #    ref_support += 0.5
-                #    reads_span_breakpoint.add(read)
-                
-                #if fa is not None:
-                #    if spans_breakpoint_mismatches (fa, input_bam, read, left_pos, left_conf_int, MIN_ALIGNED, MIN_PCT_ALIGNED):
-                #        ref_support += 0
-                #    if spans_breakpoint_mismatches (fa, input_bam, read, right_pos, right_conf_int, MIN_ALIGNED, MIN_PCT_ALIGNED):
-                #        ref_support += 0
-
-                #if clipped_by_breakpoint_old(read, left_pos, left_conf_int, right_pos, right_conf_int, SPLIT_SLOP, EXTRA_CLIP_SLOP):
-                #    alt_support += 0
-
-                ##if is_mismatched_over_sv(read, left_pos, right_pos, MISMATCH_SLOP, MISMATCH_PCT):
-                #if is_mismatched_over_sv_ver2(read, left_pos, left_conf_int, right_pos, right_conf_int, MISMATCH_PCT):
-                #    alt_support += 0
-
-                ##coverage_diff = get_coverage_diff_skip(read, left_pos, right_pos, chrom_length, READ_DEPTH_SKIP, READ_DEPTH_INTERVAL)
-                #coverage_diff = get_coverage_diff_old(read, left_pos, left_conf_int, right_pos, right_conf_int, chrom_length, READ_DEPTH_INTERVAL)
-                #if coverage_diff >= 0.8:
-                #    ref_support += 0 # min(coverage_diff - 1, 2)
-                #elif coverage_diff < 0.1:
-                #    alt_support += 0 # (1 - coverage_diff) * 0.20
-
-                # Find the number of bases covered by this read on the left breakpoint
-                #read_ref_bases_covered_left_actual = get_num_bases_covered(read, left_lower_bound, left_upper_bound)
-                #if read_ref_bases_covered_left_actual > 0:
-                #    ref_left_read_count += 1
-                #    ref_bases_covered_left_actual += read_ref_bases_covered_left_actual
-
-                # Find the number of bases covered by this read on the right breakpoint
-                #read_ref_bases_covered_right_actual = get_num_bases_covered(read, right_lower_bound, right_upper_bound)
-                #if read_ref_bases_covered_right_actual > 0:
-                #    ref_right_read_count += 1
-                #    ref_bases_covered_right_actual += read_ref_bases_covered_right_actual
-
-        # If there are splitters that have pieces near both breakpoints, they're in the same read_list
-        #for read_list in reads.values():
-        #    if split_by_breakpoint(read_list, left_pos, left_conf_int, right_pos, right_conf_int, SPLIT_SLOP):
-        #        alt_support += 1
-                
-                # If a read is classified as a splitter, then we don't want to give support to the reference for it
-                #for read in read_list:
-                #    if read in reads_span_breakpoint:
-                #        ref_support -= 0.0 # ignoring this does best on training set, 0.5 fives alt_het calls, but adds more het_alt calls
-
-        # Find the avg coverage per base for the region between the breakpoints
-        #inner_lower_bound, inner_upper_bound = get_inner_query_bounds(svtype, left_pos, left_conf_int, 
-        #                                                              right_pos, right_conf_int, INNER_READ_FETCH_FLANK)
-        #alt_bases_covered_possible = get_bases_possible_one_side(inner_lower_bound,inner_upper_bound)
-        #alt_bases_covered_actual = 0
-        #alt_read_count = 0
-
-        #for read_list in inner_reads.values():
-        #    for read in read_list:
-        #        alt_bases_covered_actual += get_num_bases_covered(read, inner_lower_bound, inner_upper_bound)
-        #        alt_read_count += 1
 
         # post processing
         if float(len(variant_features.coverage_diff)) != 0:
